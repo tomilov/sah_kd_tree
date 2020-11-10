2d83b263281c9e3e5240389a535d343fbf01cd70
Anatoliy Tomilov <tomilovanatoliy@gmail.com>
2020-11-11 01:55:34 +0500

    Fix debugging helper. Use size instead of capacity.

--- debug/QtCreator/thrusttypes.py
+++ debug/QtCreator/thrusttypes.py
@@ -4,9 +4,8 @@ import dumper
 def qdump__thrust__device_vector(d, value):
     innerType = value.type[0]
 
-    value = value["m_storage"]
-    start = value["m_begin"]["m_iterator"]["m_iterator"].pointer()
     size = int(value["m_size"])
+    start = value["m_storage"]["m_begin"]["m_iterator"]["m_iterator"].pointer()
 
     d.putItemCount(size)
     d.putPlotData(start, size, innerType)
