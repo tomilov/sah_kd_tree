5b8bc2882ef77d9708090c7d842d66a9de550025
Anatoliy Tomilov <tomilovanatoliy@gmail.com>
2023-01-05 01:50:48 +0500

    minor changes

--- src/utils/assert.hpp
+++ src/utils/assert.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <fmt/format.h>
+#include <spdlog/common.h>
 
 #include <string_view>
 
@@ -18,15 +19,15 @@ inline constexpr bool kEnableAssert = true;
 namespace impl
 {
 
-void assertFailed [[noreturn]] (const char * expression, const char * file, int line, const char * function, std::string_view message) UTILS_EXPORT;
-void throwInvariantError [[noreturn]] (const char * expression, const char * file, int line, const char * function, std::string_view message) UTILS_EXPORT;
+void assertFailed [[noreturn]] (const char * expression, spdlog::source_loc source_loc, std::string_view message) UTILS_EXPORT;
+void throwInvariantError [[noreturn]] (const char * expression, spdlog::source_loc source_loc, std::string_view message) UTILS_EXPORT;
 
 }  // namespace impl
 
 }  // namespace utils
 
 // clang-format off
-#define ASSERT_MSG(condition, format, ...) do if constexpr (utils::kEnableAssert) if (!(condition)) utils::impl::assertFailed(#condition, __FILE__, __LINE__, static_cast<const char *>(__FUNCTION__), fmt::vformat(FMT_STRING(format), fmt::make_format_args(__VA_ARGS__))); while (false)
+#define ASSERT_MSG(condition, format, ...) do if constexpr (utils::kEnableAssert) if (!(condition)) utils::impl::assertFailed(#condition, {__FILE__, __LINE__, static_cast<const char *>(__FUNCTION__)}, fmt::vformat(FMT_STRING(format), fmt::make_format_args(__VA_ARGS__))); while (false)
 #define ASSERT(condition) ASSERT_MSG(condition, "")
-#define INVARIANT(condition, format,  ...) do if (!(condition)) { utils::impl::throwInvariantError(#condition, __FILE__, __LINE__, static_cast<const char *>(__FUNCTION__), fmt::vformat(FMT_STRING(format), fmt::make_format_args(__VA_ARGS__))); } while (false)
+#define INVARIANT(condition, format,  ...) do if (!(condition)) { utils::impl::throwInvariantError(#condition, {__FILE__, __LINE__, static_cast<const char *>(__FUNCTION__)}, fmt::vformat(FMT_STRING(format), fmt::make_format_args(__VA_ARGS__))); } while (false)
 // clang-format on
